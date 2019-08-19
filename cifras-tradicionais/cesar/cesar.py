4c9c95dbfde5a669aee2f11ca96f0644191d31ca
Ítalo Vieira <italogv@hotmail.com>
2019-08-19 20:12:46 -0300

    Cesar Cipher

---
+++ cifras-tradicionais/cesar/cesar.py
@@ -0,0 +1,31 @@
+import sys
+
+
+CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ?,'
+
+
+def rotate(n, c):
+    try:
+        i = CHARS.index(c)
+        return CHARS[(i + n) % len(CHARS)]
+    except ValueError:
+        return c
+
+
+def transform(n, msg):
+    return [rotate(n, c) for c in msg]
+
+
+def main():
+    msg = sys.stdin.readlines()
+
+    for i in range(1, len(CHARS)):
+        print()
+        for line in msg:
+            print(''.join(transform(i, line)))
+        print()
+        print('############################################################')
+
+
+if __name__ == '__main__':
+    main()
